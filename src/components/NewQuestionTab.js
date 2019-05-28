997b158674092a4269a8574e05e4e1332114441f
FinnWestergren <fwesterg@skidmore.edu>
2019-05-28 18:58:39 -0400

    new questons tweaks

--- src/components/NewQuestionTab.js
+++ src/components/NewQuestionTab.js
@@ -1,20 +1,27 @@
-import React, { useRef } from 'react';
+import React, { useRef, useState } from 'react';
 import { connect } from 'react-redux';
 import { _saveQuestion } from '../_DATA';
 import { addQuestion } from '../actions/questions'
+import history from '../history';
 
 const NewQuestionTab = (props) => {
     const { currentUser, addQuestion } = props;
+    const [disableSubmit, setDisableSubmit] = useState(true);
     const optionOne = useRef(null);
     const optionTwo = useRef(null);
-
+    const handleChange = () => setDisableSubmit(optionOne.current.value === '' || optionTwo.current.value === '');
+    
     const handleSubmit = (e) => {
         e.preventDefault();
         _saveQuestion({
             author: currentUser.id,
             optionOneText: optionOne.current.value,
             optionTwoText: optionTwo.current.value
-        }).then(data => addQuestion(data));
+        }).then(data => {
+            addQuestion(data);
+            console.log(data);
+            history.push(`/questions/${data.id}`);
+        });
     };
 
     return (
@@ -28,12 +35,15 @@ const NewQuestionTab = (props) => {
                 <div>Would you rather...</div>
                 <input 
                     ref={optionOne}
+                    onChange={handleChange}
                     placeholder="enter first option"/>
                 <input
                     ref={optionTwo}
+                    onChange={handleChange}
                     placeholder="enter second option"/>
                 <button
-                    type="submit">Submit</button>
+                    type="submit"
+                    disabled={disableSubmit}>Submit</button>
             </form>
         </div>
     );
